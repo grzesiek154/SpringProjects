90fb30f3406b19f6d7c517879b80601f96407f94
grzesiek154 <gmalarski91@gmail.com>
2021-03-22 07:20:28 +0100

    dynamic component poc added

--- AngularExamples/angular-poc/src/app/app.module.ts
+++ AngularExamples/angular-poc/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapTestComponent } from './components/map-test/map-test.component';
+import { AlertComponent } from './dynamic-components/alert/alert.component';
+import { DynamicComponentAppComponent } from './dynamic-components/dynamic-component-app/dynamic-component-app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapTestComponent
+    MapTestComponent,
+    AlertComponent,
+    DynamicComponentAppComponent
   ],
   imports: [
     BrowserModule,
